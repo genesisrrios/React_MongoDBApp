65d05bf3d0caa3ee8f8c8f9c8b4335d9e9f1abe4
grivera <grivera@multisystems.com>
2021-03-20 10:29:22 -0400

    Added ajax call in App.js component to api

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Header from './Header';
 import ContestPreview from './ContestPreview';
+import axios from 'axios';
 
 class App extends React.Component {
 	state = {
-		pageHeader: 'Naming Contests'
+		pageHeader: 'Naming Contests',
+		contests: []
 	};
 	componentDidMount () {
+		axios.get('/api/contests')
+			.then(resp => {
+				this.setState({
+					contests: resp.data.contests
+				})
+			})
+			.catch(console.error)
 	};
 	componentWillUnmount () {
 	};
@@ -17,8 +26,8 @@ class App extends React.Component {
 				<div className="row">
 					<div className="col-md-12">
 					{
-						this.props.contests.map(
-							contest => <ContestPreview {...contest}/>
+						this.state.contests.map(
+							contest => <ContestPreview key={contest.id} {...contest}/>
 						)
 					}
 					</div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App';
-const json = require('./testData.json');
 
 ReactDom.render(
-	<App contests={json.contests} />,
+	<App />,
 	document.getElementById('root')
 );
